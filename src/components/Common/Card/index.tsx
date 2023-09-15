bee2658a80222d8a3de85743ae809e931e62cd60
Bruna <brunadias_30@hotmail.com>
2023-09-15 20:02:28 -0300

    estilização componente card

--- src/components/Common/Card/index.tsx
+++ src/components/Common/Card/index.tsx
@@ -1,37 +1,62 @@
 import React, { ReactNode } from 'react';
-import { View, StyleSheet, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, TouchableOpacityProps, Image, Text } from 'react-native';
 
 interface CardProps extends TouchableOpacityProps {
-    children: ReactNode;
+    image: string;
+    title: string;
+    nSerie: string;
 }
 
-const Card: React.FC<CardProps> = ({ children, style }) => {
+const Card: React.FC<CardProps> = ({ title, style, image, nSerie }) => {
     return (
-        <>
-            <TouchableOpacity
-                //   onPress={onPress}
-                style={[styles.card, style]}
-            >
-                <View style={styles.inner}>{children}</View>
-            </TouchableOpacity>
-
-            <View style={styles.inner}>{children}</View></>
+        <TouchableOpacity
+            //   onPress={onPress}
+            style={[styles.card, style]}
+        >
+            <View style={styles.inner}>
+                <Image style={styles.imagem} source={require('C:/API5Semestre/src/assets/image/equipa_vazio.png')} />
+                <View style={styles.legenda}>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.subtitle}>N°Serie: {nSerie}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
     card: {
-        width: 200,
+        width: 170,
         height: 200,
-        backgroundColor: '#d5d5d5',
+        backgroundColor: '#d9d9d9',
         borderRadius: 16,
         margin: 5,
-        marginTop: 15
+        marginTop: 15,
+        paddingTop: 5
     },
     inner: {
         width: '100%',
         height: '100%',
     },
+    legenda: {
+        flex: 1, 
+        justifyContent: 'center',
+        paddingHorizontal: 5,
+        backgroundColor: 'white',
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+    },
+    imagem: {
+        borderRadius: 10,
+        width: '100%',
+        height: '75%'
+    },
+    title: {
+        color: 'black',
+    },
+    subtitle: {
+        color: 'gray',
+    }
 });
 
 export default Card;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { CustomButton } from "../components/Common/Button";
+import Card from "../components/Common/Card";
 
-export const Home = ({ navigation }:any) => {
+export const Home = ({ navigation }: any) => {
     const goToScreen2 = () => {
         navigation.push("Gestão de Equipamentos");
     };
 
     return (
-        <View >
-            <Text>Home</Text>
-            <CustomButton
+        <View style={styles.container}>
+            {/* <CustomButton
                 color="#0fa36b"
                 title="Navigate to screen 2"
-                onPress={goToScreen2}/>
+                onPress={goToScreen2}/> */}
+            <Card title='Transformador' nSerie='123458879' image={""} ></Card>
+            <Card title='Transformador' nSerie='123458879' image={""} ></Card>
+            <Card title='Transformador' nSerie='123458879' image={""} ></Card>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        marginLeft: 5
+    }
+})
