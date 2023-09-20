94817e5b5159d573f8b05c96407329caa59679ca
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-09-20 08:18:46 -0300

    Criando validações de campos

--- src/screens/Usuarios.tsx
+++ src/screens/Usuarios.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { UsuariosComponente } from "../components/Usuarios";
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { apiurl } from "../Helpers/ApiUrl";
 
 
@@ -20,23 +20,149 @@ export const Usuarios = ({ navigation }: any) => {
     })
 
     const [valida, setValida] = useState(false)
-
-    const onChangeText = (name: any, value: any) => {
+    const [validaSenha, setValidaSenha] = useState(false);
+    const [validaSenhaRegex, setValidaSenhaRegex] = useState(false)
+    const [validarEmailRegex, setVlidarEmailRegex] = useState(false)
+    const [validarTexto, setValidarTexto] = useState(false)
+    const [validaTelefoneCeleluar, setValidarTelefoneCelular] = useState(false)
+    const [validaTelefoneFixo, setValidarTelefoneFixo] = useState(false)
+    const [validaMatricula, setValidarMatricula] = useState(false)
+    const [validaMatriculaRegex, setValidarMatriculaRegex] = useState(false)
+    const [validaCpfRegex, setValidarCpfRegex] = useState(false)
+    const onChangeText = (name: string, value: string) => {
         onChangeForm({ ...form, [name]: value });
 
 
     };
+    function validarVazio(nome: string, sobrenome: string, email: string, telefone1: string, matricula: string, cpf: string, senha: string) {
+        if (!nome || !sobrenome || !email || !telefone1 || !matricula || !cpf || !senha) {
+            setValida(true)
+            return true
+        }
+        setValida(false)
+        return false
+    }
+    function validaTextoSemNmr(nome: string, sobrenome: string) {
+        const textoRegex = /^[a-zA-Z]+$/;
+        if (!textoRegex.test(nome) || !textoRegex.test(sobrenome)) {
+            setValidarTexto(true)
+            return true
+        }
+        setValidarTexto(false)
+        return false
+    }
 
+    function validarTelefone(telefone: string) {
+        const celularRegex = /^\d{11}$/;
+        if (!celularRegex.test(telefone)) {
+            setValidarTelefoneCelular(true)
+            return true
+        }
+        setValidarTelefoneCelular(false)
+        return false
+    }
+    function validarTelefoneFixo(telefone: string) {
+        const regexTelefoneFixo = /^\d{10}$/;
+        if (telefone) {
+            if (!regexTelefoneFixo.test(telefone)) {
+                setValidarTelefoneFixo(true)
+                return true
+            }
+            setValidarTelefoneFixo(false)
+            return false
+        }
+        setValidarTelefoneFixo(false)
+        return false
+
+    }
+
+    function validarSenha(senha: string) {
+        if (senha.length <= 10 || senha.length >= 20) {
+            setValidaSenha(true);
+            return true
+        }
+        setValidaSenha(false)
+        return false
+    }
+    function validarSenhaRegex(senha: string) {
+        const senhaRegex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).{8,}$/;
+        if (!senhaRegex.test(senha)) {
+            setValidaSenhaRegex(true);
+            return true
+        }
+        setValidaSenhaRegex(false)
+        return false
+    }
+    function validarEmail(email: string) {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            setVlidarEmailRegex(true)
+            return true
+        }
+        setVlidarEmailRegex(false)
+        return false
+    }
+    function validarMatricula(matricula: string) {
+        if (matricula.length < 5) {
+            setValidarMatricula(true)
+            return true
+        }
+        setValidarMatricula(false)
+        return false
+    }
+    function validarMatriculaRegex(matricula: string) {
+        const matriculaRegex = /^\d{5,15}$/;
+        if (!matriculaRegex.test(matricula)) {
+            setValidarMatriculaRegex(true)
+            return true
+        }
+        setValidarMatriculaRegex(false)
+        return false
+    }
+    function validarCpfRegex(cpf: string) {
+        const CPFRegex = /^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+        if (!CPFRegex.test(cpf)) {
+            setValidarCpfRegex(true)
+            return true
+        }
+        setValidarCpfRegex(false)
+        return false
+    }
 
     function cadastrarUsuario() {
-
-        if (!form.nome) {
-            setValida(true)
+        if (validarVazio(form.nome, form.sobrenome, form.email, form.telefone1, form.matricula, form.cpf, form.senha)) {
             return
-        } else {
-            setValida(false)
-
         }
+        if (validaTextoSemNmr(form.nome, form.sobrenome)) {
+            return
+        }
+        if (validarEmail(form.email)) {
+            return
+        }
+        if (validarTelefone(form.telefone1)) {
+            return
+        }
+        if (validarTelefoneFixo(form.telefone2)) {
+            return
+        }
+        if (validarMatricula(form.matricula)) {
+            return
+        }
+        if (validarMatriculaRegex(form.matricula)) {
+            return
+        }
+        if (validarCpfRegex(form.cpf)) {
+            return
+        }
+        if (validarSenha(form.senha)) {
+            return
+        }
+        if (validarSenhaRegex(form.senha)) {
+            return
+        }
+
+
+
         const url = apiurl + "/user/create";
         fetch(url, {
             method: 'POST',
@@ -59,19 +185,68 @@ export const Usuarios = ({ navigation }: any) => {
             })
     }
 
+function cancelar(){
+    navigation.navigate("Usuários");
+
+}
 
     return (
         <>
             {valida ?
-                <Text style={{ color: "red" }}>Campos com * são obrigatórios.</Text>
+                <Text style={{ color: "red", paddingLeft: 12 }}>Campos com * são obrigatórios.</Text>
+                : ""
+            }
+            {validaSenha ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>A senha deve ter entre 10 e 20 caracteres.</Text>
+                : ""
+            }
+            {validaSenhaRegex ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>A senha deve conter uma letra Maiuscula, um caracter especial e numeros entre 0 e 9.</Text>
+                : ""
+            }
+            {validarEmailRegex ?
+                <View>
+                    <Text style={{ color: "red", paddingLeft: 12 }}>O e-mail deve conter os seguintes itens:</Text>
+                    <Text style={{ color: "red", paddingLeft: 12 }}>Pelo menos um caractere antes do '@'</Text>
+                    <Text style={{ color: "red", paddingLeft: 12 }}>Pelo menos um caractere antes do ponto '.' no domínio</Text>
+                    <Text style={{ color: "red", paddingLeft: 12 }}>O domínio deve conter pelo menos duas letras (por exemplo, 'com', 'org', 'net')</Text>
+                    <Text style={{ color: "red", paddingLeft: 12 }}>Não deve conter espaços em branco</Text>
+                </View>
+                : ""
+            }
+            {validarTexto ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>Nome ou sobrenome deve conter apenas letras.</Text>
+                : ""
+            }
+            {validaTelefoneCeleluar ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>O número do celular deve ter 11 números.</Text>
+                : ""
+            }
+            {validaTelefoneFixo ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>O telefone de recado deve ter 10 números.</Text>
+                : ""
+            }
+            {validaMatricula ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>A matricula deve conter no minimo 5 letras.</Text>
+                : ""
+            }
+            {validaMatriculaRegex ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>A matricula deve conter apenas números.</Text>
+                : ""
+            }
+            {validaCpfRegex ?
+                <Text style={{ color: "red", paddingLeft: 12 }}>O CPF deve conter o padrão xxx.xxx.xxx-xx.</Text>
                 : ""
             }
             <UsuariosComponente
                 form={form}
                 onChangeText={onChangeText}
                 onPress={cadastrarUsuario}
+                onpress2={cancelar}
+                title2={'Cancelar'}
                 title={'Cadastrar'}
                 color={'steelblue'}
+                color2={'steelblue'}
 
             />
         </>
