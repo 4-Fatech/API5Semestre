09265db132a4bdde52b0092c96cb488eac654bf0
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-09-20 08:16:51 -0300

    Retirando a importação do componente

--- src/components/Common/Card/index.tsx
+++ src/components/Common/Card/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity, TouchableOpacityProps, Image, Text } from 'react-native';
-import { EquipamentoVazio } from '../../../Assets/image';
 
 interface CardProps extends TouchableOpacityProps {
     image: string;
@@ -18,7 +17,7 @@ const Card: React.FC<CardProps> = ({ title, style, image, id, onCardPress }) => 
             <View style={styles.inner}>
                 <Image
                     style={styles.imagem}
-                    source={image ? { uri: image } : EquipamentoVazio}
+                    source={image ? { uri: image } : {}}
                 />
                 <View style={styles.legenda}>
                     <Text style={styles.title}>{title}</Text>
