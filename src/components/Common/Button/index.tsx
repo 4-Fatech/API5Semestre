d0187559fae05b1abccc6240bc52bbd5a73c2ee1
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-09-20 10:49:08 -0300

    Refatorando botão

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import {StyleSheet, Text, Pressable, ViewStyle, TextStyle} from 'react-native';
 
 interface ButtonProps {
     title: string,
@@ -9,16 +9,47 @@ interface ButtonProps {
 
 export const CustomButton: React.FC<ButtonProps> = ({ title, onPress, color }) => {
     return (
-        <Button
-
-            color={color}
-            title={title}
+        <Pressable
             onPress={onPress}
+            style={({pressed}) => [
+                {
+                  backgroundColor: pressed ? color : 'steelblue',
+                },
+                styles.container,
+              ]}>
+        
+            <Text>{title}</Text>
+        </Pressable >
+        // <Button
+        //     borderRadius= '2'
+        //     color={color}
+        //     title={title}
+        //     onPress={onPress}
 
-        ></Button>
+        // ></Button>
     );
 }
 
-
+interface Styles {
+    container: ViewStyle;
+    text: TextStyle;
+  }
+  
+const styles = StyleSheet.create<Styles>({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 45,
+      minWidth: '45%',
+      marginHorizontal: 8,
+      marginVertical: 4,
+      borderRadius: 8,
+    },
+    text: {
+      textAlign: 'center',
+      color: 'white',
+    },
+  });
 
 
