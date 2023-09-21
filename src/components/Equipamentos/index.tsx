e77e59cac9925b2a12bac65bda53f5cca3555535
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-09-21 10:44:30 -0300

    Adicionando a props na label e nos botões

--- src/components/Equipamentos/index.tsx
+++ src/components/Equipamentos/index.tsx
@@ -1,11 +1,11 @@
-import React , {useEffect}from "react";
+import React, { useEffect } from "react";
 import { View, ScrollView } from "react-native";
 import { Label } from "../Common/Label/Label";
 import { Input } from "../Common/Input/Input";
 import ImageInput from "../Common/ImageInput/ImageInput";
 import { CustomButton } from "../Common/Button";
 
-export const EquipamentosComponente = ({ onChangeText, form, onPress, title, color }: any) => {
+export const EquipamentosComponente = ({ onChangeText, form, onPress,onpress2, title, title2, color, color2, color3,color4,corTexto }: any) => {
   useEffect(() => {
 
     onChangeText('latitude', '');
@@ -15,13 +15,13 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
     onChangeText('status', '');
     onChangeText('tipo', '');
     onChangeText('modelo', '');
- 
-}, []);
+
+  }, []);
 
   return (
     <ScrollView>
       <View >
-        <Label titulo='Tipo do Equipamento' />
+        <Label titulo='Tipo do Equipamento' requirido="" />
         <Input
           onChangeText={(value: any) => {
             onChangeText('tipo', value);
@@ -31,7 +31,7 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
         />
 
         {/*  */}
-        <Label titulo='Modelo do Equipamento' />
+        <Label titulo='Modelo do Equipamento' requirido=""/>
         <Input
           onChangeText={(value: any) => {
             onChangeText('modelo', value);
@@ -41,7 +41,7 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
         />
 
         {/*  */}
-        <Label titulo='N° de Série' />
+        <Label titulo='N° de Série' requirido="" />
         <Input
           onChangeText={(value: any) => {
             onChangeText('serial', value);
@@ -51,7 +51,7 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
         />
 
         {/*  */}
-        <Label titulo='Latitude' />
+        <Label titulo='Latitude' requirido=""/>
         <Input
           onChangeText={(value: any) => {
             onChangeText('latitude', value);
@@ -61,7 +61,7 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
         />
 
         {/*  */}
-        <Label titulo='Longitude' />
+        <Label titulo='Longitude' requirido=""/>
         <Input
           onChangeText={(value: any) => {
             onChangeText('longitude', value);
@@ -70,7 +70,7 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
           placeholder="Ex.: 45.22837"
         />
         {/*  */}
-        <Label titulo='Observações' />
+        <Label titulo='Observações' requirido="" />
         <Input
           onChangeText={(value: any) => {
             onChangeText('observacoes', value);
@@ -79,13 +79,13 @@ export const EquipamentosComponente = ({ onChangeText, form, onPress, title, col
           placeholder="Ex.: Equipamento localizado próximo a uma esquina" />
 
         {/*  */}
-        <Label titulo='Imagens do equipamento' />
-        <ImageInput 
-            form={form}
-            onChange={onChangeText}
-          />
-        <CustomButton  title={title} onPress={onPress} color={color} />
-      </View>
+        <Label titulo='Imagens do equipamento' requirido="" />
+        <ImageInput
+          form={form}
+          onChange={onChangeText}
+        />
+        <CustomButton title={title2} corTexto={corTexto} onPress={onpress2} color={color3} color2={color4} />
+        <CustomButton title={title} corTexto={corTexto} onPress={onPress} color={color} color2={color2} />      </View>
     </ScrollView>
   );
 };
