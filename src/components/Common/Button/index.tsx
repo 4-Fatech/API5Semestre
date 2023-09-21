9649576fc78a56a47966ad67d2026b434d22870f
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-09-21 10:43:31 -0300

    Adicionando props para melhorar a estilização do componente

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -1,55 +1,50 @@
 import React from "react";
-import {StyleSheet, Text, Pressable, ViewStyle, TextStyle} from 'react-native';
+import { StyleSheet, Text, Pressable, ViewStyle, TextStyle } from 'react-native';
 
 interface ButtonProps {
     title: string,
     onPress: any,
-    color: string
+    color: string,
+    color2: string,
+    corTexto: string
 }
 
-export const CustomButton: React.FC<ButtonProps> = ({ title, onPress, color }) => {
+export const CustomButton: React.FC<ButtonProps> = ({ title, onPress, color, color2, corTexto }) => {
     return (
         <Pressable
             onPress={onPress}
-            style={({pressed}) => [
+            style={({ pressed }) => [
                 {
-                  backgroundColor: pressed ? color : 'steelblue',
+                    backgroundColor: pressed ? color : color2,
                 },
                 styles.container,
-              ]}>
-        
-            <Text>{title}</Text>
-        </Pressable >
-        // <Button
-        //     borderRadius= '2'
-        //     color={color}
-        //     title={title}
-        //     onPress={onPress}
+            ]}>
 
-        // ></Button>
+            <Text style={{color:corTexto}}>{title}</Text>
+        </Pressable >
     );
 }
 
 interface Styles {
     container: ViewStyle;
     text: TextStyle;
-  }
-  
+}
+
 const styles = StyleSheet.create<Styles>({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: 45,
-      minWidth: '45%',
-      marginHorizontal: 8,
-      marginVertical: 4,
-      borderRadius: 8,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 45,
+        minWidth: '45%',
+        marginHorizontal: 8,
+        marginVertical: 4,
+        borderRadius: 8,
     },
     text: {
-      textAlign: 'center',
-      color: 'white',
+        textAlign: 'center',
+        color: 'white',
     },
-  });
+});
 
 
