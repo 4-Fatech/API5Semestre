941713dea29c6f1e28553bd332f8d3ca5c9bfcb9
Antonio Marcelo <antoniomarcelo545@gmail.com>
2023-10-02 10:46:32 -0300

    Modal listar equipamentos

--- src/screens/Equipamentos/ListarEquipamentos.tsx
+++ src/screens/Equipamentos/ListarEquipamentos.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View, TextInput} from "react-native";
+import { ScrollView, StyleSheet, Text, View, TextInput, Alert} from "react-native";
 import Card from "../../components/Common/Card";
 import { CustomButton } from "../../components/Common/Button";
 import { apiurl } from "../../Helpers/ApiUrl";
@@ -58,9 +58,25 @@ export const Home = ({ route, navigation }: any) => {
         }
     }, [equipAlterada, equipCadastrada]);
 
-    const handleCardPress = (id: string) => {
-        navigation.navigate("Atualizar Equipamento", { id });
-    };
+    // const handleCardPress = (id: string) => {
+    //     navigation.navigate("Atualizar Equipamento", { id });
+    // };
+
+    const showAlert = (id: string) => {
+        Alert.alert(
+          'Ações',
+          'O que deseja fazer.',
+          [
+            {
+              text: 'Editar',
+              onPress: () =>   navigation.navigate("Atualizar Equipamento", { id }),
+              style: 'cancel',
+            },
+            { text: 'Visualizar', onPress: () => console.log('Ir para tela detalhes') },
+          ],
+          { cancelable: false }
+        );
+      };
     const filteredEquipamento = equipamento.filter((equipamento) => {
         return equipamento.tipo.toLowerCase().includes(searchText.toLowerCase());
     });
@@ -83,7 +99,7 @@ export const Home = ({ route, navigation }: any) => {
                             id={e.id}
                             image={e.foto}
                             ativo={e.status}
-                            onCardPress={handleCardPress}>
+                            onCardPress={showAlert}>
                         </Card>
                     )}
                 </View>
