40e432b80cbe15b606fdea4fbc3c9b1a19e47ab0
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-10-07 21:39:29 -0300

    Feature: Deixando a scrollview na horizontal

--- src/components/Common/ImageInput/ImageInput.tsx
+++ src/components/Common/ImageInput/ImageInput.tsx
@@ -33,7 +33,7 @@ export default function ImageInput({ form, onChange }: any) {
     }
   });
   const handleDeleteImage = (index: any) => {
-    if (Array.isArray(form.foto) && index >= 0 && index < form.foto.length) {      
+    if (Array.isArray(form.foto) && index >= 0 && index < form.foto.length) {
       const updatedFoto = [...form.foto.slice(0, index), ...form.foto.slice(index + 1)];
       onChange("foto", updatedFoto);
     }
@@ -41,55 +41,61 @@ export default function ImageInput({ form, onChange }: any) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.buttonContainer}>
-          {actions.map(({ title, type, options }) => {
-            return (
-              <DemoButton
-                key={title}
-                onPress={() => onButtonPress(type, options)}>
-                {title}
-              </DemoButton>
-            );
-          })}
-        </View>
-        {form.foto &&
-          Array.isArray(form.foto) && form.foto.map((uri: any) =>
-            <View key={uri} style={styles.imageContainer}>
-              <Image
-                resizeMode="cover"
-                resizeMethod="scale"
-                style={styles.image}
-                source={{ uri: uri }}
-              />
+      <View style={styles.buttonContainer}>
+        {actions.map(({ title, type, options }) => {
+          return (
+            <DemoButton
+              key={title}
+              onPress={() => onButtonPress(type, options)}>
+              {title}
+            </DemoButton>
+          );
+        })}
+      </View>
+      <View style={styles.alinhamentoCentro}>
 
-              <TouchableOpacity
-                onPress={() => {
-                  Alert.alert(
-                    'Remover imagem',
-                    'Deseja remover imagem?',
-                    [
-                      {
-                        text: 'NÃO',
-                        onPress: () => null,
-                        style: 'cancel',
-                      },
-                      { text: 'SIM', onPress: () => handleDeleteImage(form.foto.indexOf(uri)) },
-                    ],
-                    { cancelable: false }
-                  );
+        <ScrollView
+          horizontal={true}
+          contentContainerStyle={styles.scrollViewContent}
+        >
+          {form.foto &&
+            Array.isArray(form.foto) && form.foto.map((uri: any) =>
+              <View key={uri} style={styles.imageContainer}>
+                <Image
+                  resizeMode="cover"
+                  resizeMethod="scale"
+                  style={styles.image}
+                  source={{ uri: uri }}
+                />
+
+                <TouchableOpacity
+                  onPress={() => {
+                    Alert.alert(
+                      'Remover imagem',
+                      'Deseja remover imagem?',
+                      [
+                        {
+                          text: 'NÃO',
+                          onPress: () => null,
+                          style: 'cancel',
+                        },
+                        { text: 'SIM', onPress: () => handleDeleteImage(form.foto.indexOf(uri)) },
+                      ],
+                      { cancelable: false }
+                    );
                   }
-                } 
-                style={styles.deleteButton}
-              >
-                <Text style={styles.deleteButtonText}>Remover</Text>
-              </TouchableOpacity>
+                  }
+                  style={styles.deleteButton}
+                >
+                  <Text style={styles.deleteButtonText}>Remover</Text>
+                </TouchableOpacity>
 
-            </View>
+              </View>
 
-          )
-        }
-      </ScrollView>
+            )
+          }
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -98,13 +104,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  scrollViewContent: {
+    flexDirection: 'row',
+  },
+  alinhamentoCentro: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   buttonContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginVertical: 8,
   },
   imageContainer: {
+    marginRight: 7,
     marginVertical: 24,
+    width: 250,
     alignItems: 'center',
   },
   image: {
@@ -112,7 +128,7 @@ const styles = StyleSheet.create({
     height: 200,
   },
   deleteButton: {
-    backgroundColor:  '#94C021',
+    backgroundColor: '#94C021',
     padding: 10,
     borderRadius: 5,
     marginTop: 8,
