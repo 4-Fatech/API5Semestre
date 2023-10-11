06359a3071c3642e29bd0c54f3c3ed7e9a843af8
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-10-11 08:08:15 -0300

    feature: criando a rota para o perfil

--- src/navigations/DrawerNavigator.tsx
+++ src/navigations/DrawerNavigator.tsx
@@ -7,6 +7,8 @@ import { RecuperarSenha } from '../components/Login/Recuperacao';
 import { PagCodigo } from '../components/Login/Recuperacao/PagCodigo';
 import { AtualizarSenha } from '../components/Login/Recuperacao/AtualizarSenha';
 import Logout from '../screens/Logout';
+import { StyleSheet, Text, View } from 'react-native';
+import UsuarioStackNavigatorPerfil from './UsuarioStackNavigatorPerfil';
 
 
 
@@ -16,9 +18,10 @@ export default function DrawerNavigator() {
     <Drawer.Navigator>
       <Drawer.Screen name='Equipamentos ' component={StackNavigator} options={{ headerShown: false }} />
       <Drawer.Screen name='Usuários ' component={UsuarioStackNavigator} options={{ headerShown: false }} />
+      <Drawer.Screen name='Pefil ' component={UsuarioStackNavigatorPerfil} options={{ headerShown: false }} />
       <Drawer.Screen name='Sair' component={Logout} options={{ headerShown: false }} />
-      
-      
+
+
     </Drawer.Navigator>
   );
 }
