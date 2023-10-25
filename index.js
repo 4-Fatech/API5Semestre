1472c1d5c0a17dd8977f7919ee0550c15caaed26
Bruna <brunadias_30@hotmail.com>
2023-10-25 07:37:06 -0300

    configurando chave e render

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
+import { enableLatestRenderer } from 'react-native-maps';
 
 AppRegistry.registerComponent('api', () => App);
 
 if (Platform.OS === 'web') {
     const rootTag = document.getElementById('root') || document.getElementById('X');
     AppRegistry.runApplication('api', { rootTag });
+    enableLatestRenderer();
 }
